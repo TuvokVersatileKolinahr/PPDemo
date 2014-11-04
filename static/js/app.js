ca3e8a651b6fd16d1be9ddb9fdb2da905137f07f
elger <elgervb@gmail.com>
2014-11-04 21:21:09 +0100

    basic project setup

---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+var app = angular.module('propertypassport', [])
+
+  .controller('MainController', ['$scope', function($scope){
+
+    
+
+  }]);
---
+++ static/js/mock.js
@@ -0,0 +1,27 @@
+[
+	{
+		Code			: "Baggerweg Scheveningen",
+		Name			: "OBJ0371",
+		PhotoPhileRef	: "http://www.castles.nl/tn_p8280232.jpg"
+	},
+	{
+		Code			: "Baggerweg Scheveningen",
+		Name			: "OBJ0371",
+		PhotoPhileRef	: "http://www.castles.nl/tn_p8280232.jpg"
+	},
+	{
+		Code			: "Baggerweg Scheveningen",
+		Name			: "OBJ0371",
+		PhotoPhileRef	: "http://www.castles.nl/tn_p8280232.jpg"
+	},
+	{
+		Code			: "Baggerweg Scheveningen",
+		Name			: "OBJ0371",
+		PhotoPhileRef	: "http://www.castles.nl/tn_p8280232.jpg"
+	},
+	{
+		Code			: "Baggerweg Scheveningen",
+		Name			: "OBJ0371",
+		PhotoPhileRef	: "http://www.castles.nl/tn_p8280232.jpg"
+	}
+]
