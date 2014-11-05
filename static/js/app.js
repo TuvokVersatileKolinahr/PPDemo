6cd4cd88934421aaa9c93286d554c344d0d78e5f
elger <elgervb@gmail.com>
2014-11-05 13:52:48 +0100

    Jump to Marker on map when clicking an item in the list
    fixes #1

--- static/js/app.js
+++ static/js/app.js
@@ -1,32 +1,62 @@
 var app = angular.module('propertypassport', [])
 
   .controller('MainController', ['$scope', '$http', function($scope, $http){
-    var map;
-	  $http.get('static/js/mock.js').
-	  success(function(data, status, headers, config) {
-	   	$scope.properties = data.result;
+    var map, markers = [];
 
-	   	console.log('Got ' + $scope.properties.length + ' properties');
+    /**
+     * Zoom to a marker with a specific ID on the map
+     * @param string The id of the marker
+     */
+    $scope.zoomToMarker = function(id){
+      var marker;
+      markers.forEach(function(m){
+        if (m.id == id){
+          marker = m;
+        }
+      });
+
+      if (marker){
+        map.setCenter(marker.getPosition());
+        map.setZoom(18);
+      }
+    };
+
+    // Get the data from the server...
+    $http.get('static/js/mock.js').
+    success(function(data, status, headers, config) {
+      $scope.properties = data.result;
+
+      console.log('Got ' + $scope.properties.length + ' properties');
 
       for (var t = 0; t < data.result.length; t++) {
-        console.log("comment", data.result[t].comment.split(","));
+        var property = $scope.properties[t];
+        
         var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(data.result[t].comment.split(",")[0], data.result[t].comment.split(",")[1]),
-          title: data.result[t].name,
+          position: new google.maps.LatLng(property.comment.split(",")[0], property.comment.split(",")[1]),
+          id: property.code, // set the ID for lookup purposes, @see zoomToMarker
+          title: property.name,
           map: map
         });
+        markers.push(marker);
+
+        google.maps.event.addListener(marker, 'click', function() {               
+          // TODO select the property in the list
+        });
+        
       }
-	  }).
-	  error(function(data, status, headers, config) {
-	    console.error('Could not retrieve properties from server...');
-	  });
+
+    }).
+    error(function(data, status, headers, config) {
+      console.error('Could not retrieve properties from server...');
+    });
 
 
-	  /* Init maps */
-	  function initialize() {
+    /* Init maps */
+    function initialize() {
         var mapOptions = {
           center: { lat: 51.5873617, lng: 4.7663469},
           zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
           disableDefaultUI: true
         };
         map = new google.maps.Map(document.getElementById('map-canvas'),
