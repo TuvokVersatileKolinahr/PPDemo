b33d26b0372517ae877ba2f4cc3598a10f290dfd
Wouter <tuvokki@gmail.com>
2014-11-06 22:53:01 +0100

    Added configuration setting
    Just set ‘localdev’ to true to prevent an actual server call

--- static/js/app.js
+++ static/js/app.js
@@ -1,83 +1,106 @@
+angular.module('propertypassport.config',[])
+  .constant('config', {
+    'version': 0.2,
+    'localdev': true,
+    'baseUrl': 'http://pc06698',
+    'serviceUrl': '/REST/FrameUI/demo/demo_webclient/SensorService',
+    'executeMethod': 'getAllProperties'
+  })
 /**
  * Demo app for the Planon Property Passport
  *
  * Specs: http://2z9ox8.axshare.com/
  */
-var app = angular.module('propertypassport', [])
+var app = angular.module('propertypassport', ['propertypassport.config'])
 
-  .controller('MainController', ['$scope', '$http', function($scope, $http){
-    var map, markers = [];
+  .controller('MainController', ['$scope', '$http', 'config', function($scope, $http, config){
 
-    /**
-     * Zoom to a marker with a specific ID on the map
-     * @param string The id of the marker
-     * @return google.maps.Marker the selected marker or undefined when not found
-     */
-    $scope.selectMarker = function(id){
-      var marker;
-      markers.forEach(function(m){
-        if (m.id == id){
-          marker = m;
+  var map, markers = [];
+
+  /**
+   * Zoom to a marker with a specific ID on the map
+   * @param string The id of the marker
+   * @return google.maps.Marker the selected marker or undefined when not found
+   */
+  $scope.selectMarker = function(id){
+    var marker;
+    markers.forEach(function(m){
+      if (m.id == id){
+        marker = m;
+      }
+    });
+
+    if (marker){
+      map.panTo(marker.getPosition());
+      //map.setZoom(18);
+      $scope.properties.forEach(function(p){
+        if (p.code === marker.id){
+          $scope.selected = p;
         }
       });
-
-      if (marker){
-        map.panTo(marker.getPosition());
-        //map.setZoom(18);
-        $scope.properties.forEach(function(p){
-          if (p.code === marker.id){
-            $scope.selected = p;
-          }
-        });
-        console.log('Zoom to marker ' + id);
-      }
-      return marker;
-    };
-    $scope.selectMarkerWithInfo = function(id){
-      if ($scope.selectMarker(id)){
-        $scope.showInfo = !$scope.showInfo;
-      }
+      console.log('Zoom to marker ' + id);
     }
-    $scope.clear = function(){
-      $scope.showInfo = false;
+    return marker;
+  };
+  $scope.selectMarkerWithInfo = function(id){
+    if ($scope.selectMarker(id)){
+      $scope.showInfo = !$scope.showInfo;
     }
-  var baseUrl = "http://pc06698";
-  var serviceUrl = baseUrl + "/REST/FrameUI/demo/demo_webclient/SensorService";
-  var executeMethod = "getAllProperties";
+  }
+  $scope.clear = function(){
+    $scope.showInfo = false;
+  }
 
-  $http.post(serviceUrl, {method:executeMethod}).
-    success(function(data, status, headers, config) {
-      $scope.properties = data.result;
+  function drawMarkers (data) {
+    for (var t = 0; t < data.result.length; t++) {
+      var property = $scope.properties[t];
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(property.comment.split(",")[0], property.comment.split(",")[1]),
+        id: property.code, // set the ID for lookup purposes, @see selectMarker
+        title: property.name,
+        map: map
+      });
+      markers.push(marker);
 
-      console.log('Got ' + $scope.properties.length + ' properties');
-
-      for (var t = 0; t < data.result.length; t++) {
-        var property = $scope.properties[t];
-        var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(property.comment.split(",")[0], property.comment.split(",")[1]),
-          id: property.code, // set the ID for lookup purposes, @see selectMarker
-          title: property.name,
-          map: map
-        });
-        markers.push(marker);
-
-        // wrap inside a closure to keep reference to the right marker...
-        (function(marker){
-          google.maps.event.addListener(marker, 'click', function() {               
-            // TODO select the property in the list
-            // show side panel
-            $scope.$apply(function(){
-              $scope.showInfo = true;
-              $scope.selectMarker(marker.id);
-            });
+      // wrap inside a closure to keep reference to the right marker...
+      (function(marker){
+        google.maps.event.addListener(marker, 'click', function() {               
+          // TODO select the property in the list
+          // show side panel
+          $scope.$apply(function(){
+            $scope.showInfo = true;
+            $scope.selectMarker(marker.id);
           });
-        })(marker);
-        
-      }
-    }).
-    error(function(data, status, headers, config) {
-      console.error('Could not retrieve properties from server...');
-    });
+        });
+      })(marker);
+    }
+  }
+
+  if (config.localdev) {
+    $http.get('static/js/mock.js').
+      success(  function(data, status, headers, config) {
+        $scope.properties = data.result;
+
+        console.log('Got ' + $scope.properties.length + ' properties');
+
+        drawMarkers(data);
+      }).
+      error(function(data, status, headers, config) {
+        console.error('Could not retrieve properties from server...');
+      });
+  } else { 
+    $http.post(config.baseUrl + config.serviceUrl, {method:config.executeMethod}).
+      success(  function(data, status, headers, config) {
+        $scope.properties = data.result;
+
+        console.log('Got ' + $scope.properties.length + ' properties');
+
+        drawMarkers(data);
+      }).
+      error(function(data, status, headers, config) {
+        console.error('Could not retrieve properties from server...');
+      });
+    }
 
     /* Init maps */
     function initialize() {
