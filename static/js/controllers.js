3c90c38d8ee1039a0beec24b6a24863535924c87
Wouter Roosendaal <tuvokki@gmail.com>
2014-11-11 23:54:48 +0100

    Slightly more zoom on map

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -87,7 +87,7 @@ ppControllers.controller('MainController', ['$scope', '$http', 'config', 'dataCa
     var mapOptions = {
       // center: { lat: 51.5873617, lng: 4.7663469},//Breda
       center: { lat: 51.84873, lng: 5.86428 }, //Nijmegen
-      zoom: 15,
+      zoom: 16,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
       disableDefaultUI: true
     };
