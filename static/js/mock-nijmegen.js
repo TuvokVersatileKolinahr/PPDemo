c6106b2e280a9b3b2810cebeeab5a47de0c5504d
Wouter <tuvokki@gmail.com>
2014-11-12 00:03:20 +0100

    Example mock of Nijmegen data

---
+++ static/js/mock-nijmegen.js
@@ -0,0 +1,75 @@
+{
+  "id":0,
+  "result":[
+    {
+      "code":"OBJ0000388",
+      "comment":"51.843721,5.860812",
+      "name":"De Fuik",
+      "photoref":"D:\\Development\\pss2-head\\Servers\\tomcat\\webapps\\static\\buildings\\fuik.jpg",
+      "usage":"6"
+    },
+    {
+      "code":"OBJ0000392",
+      "comment":"51.84873,5.86428",
+      "name":"De Hemel",
+      "photoref":"D:\\Development\\pss2-head\\Servers\\tomcat\\webapps\\static\\buildings\\dehemel.jpg",
+      "usage":"5"
+    },
+    {
+      "code":"OBJ0000393",
+      "comment":"51.848644,5.869441",
+      "name":"De Stratenmakerstoren",
+      "photoref":"D:\\Development\\pss2-head\\Servers\\tomcat\\webapps\\static\\buildings\\stratenmakerstoren.jpg",
+      "usage":"2"
+    },
+    {
+      "code":"OBJ0000390",
+      "comment":"51.847394,5.861638",
+      "name":"In de Blaauwe Hand",
+      "photoref":"D:\\Development\\pss2-head\\Servers\\tomcat\\webapps\\static\\buildings\\blaauwehand.jpg",
+      "usage":"6"
+    },
+    {
+      "code":"OBJ0000386",
+      "comment":"51.845421,5.866694",
+      "name":"Marienburg",
+      "photoref":"D:\\Development\\pss2-head\\Servers\\tomcat\\webapps\\static\\buildings\\marienburg.jpg",
+      "usage":"4"
+    },
+    {
+      "code":"OBJ0000384",
+      "comment":"51.844593,5.870417",
+      "name":"Natuurmuseum",
+      "photoref":"D:\\Development\\pss2-head\\Servers\\tomcat\\webapps\\static\\buildings\\natuurmuseum.jpg",
+      "usage":"2"
+    },
+    {
+      "code":"OBJ0000389",
+      "comment":"51.845863,5.862818",
+      "name":"Plein 1944",
+      "photoref":"D:\\Development\\pss2-head\\Servers\\tomcat\\webapps\\static\\buildings\\plein44.jpg",
+      "usage":"4"
+    },
+    {
+      "code":"OBJ0000385",
+      "comment":"51.848095,5.869707",
+      "name":"St. Nicolaas kapel",
+      "photoref":"D:\\Development\\pss2-head\\Servers\\tomcat\\webapps\\static\\buildings\\valkhof.jpg",
+      "usage":"1"
+    },
+    {
+      "code":"OBJ0000387",
+      "comment":"51.844712,5.866994",
+      "name":"Stadswinkel",
+      "photoref":"D:\\Development\\pss2-head\\Servers\\tomcat\\webapps\\static\\buildings\\stadswinkel.jpg",
+      "usage":"4"
+    },
+    {
+      "code":"OBJ0000391",
+      "comment":"51.847918,5.862789",
+      "name":"Stevenskerk",
+      "photoref":"D:\\Development\\pss2-head\\Servers\\tomcat\\webapps\\static\\buildings\\stevenskerk.jpg",
+      "usage":"1"
+    }
+  ]
+}
