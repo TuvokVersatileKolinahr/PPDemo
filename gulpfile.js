941dc5365fc67d379dd46aa3b7292f5df835a8d6
elger <elgervb@gmail.com>
2014-11-17 21:24:51 +0100

    Start using gulp
    for watching files & watching browser

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+
+gulp.task('js', function(){
+ return gulp.src('static/js/*.js')
+  .pipe(concat('all.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('static/js'));
+});
+
+gulp.task('sass', function(){
+ return gulp.src('static/css/*.scss')
+        .pipe(sass())
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('static/css'))
+        .pipe(reload({stream:true}));
+});
+
+// browser-sync task for starting the server.
+gulp.task('browser-sync', function() {
+    return browserSync({
+        server: {
+            baseDir: "./"
+        },
+        ghostMode: {
+		    clicks: true,
+		    location: true,
+		    forms: true,
+		    scroll: true
+		},
+		open: "external"
+    });
+});
+
+// Default task to be run with `gulp`
+gulp.task('watch', ['sass', 'browser-sync'], function () {
+    gulp.watch("static/css/*.scss", ['sass']);
+});
